d4d4f64fd16453ee80ad6282a615b58816d2d1fe
BiostormSilver <thomas0398stevens2011@hotmail.co.uk>
2022-03-17 15:01:12 +0000

    Altered method for text update and setup components for splitting the input and update graph callbacks

--- App.py
+++ App.py
@@ -8,9 +8,10 @@ import dash
 import dash_bootstrap_components as dbc
 from dash import dcc
 from dash import html
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 from string import ascii_uppercase
 from plotly.subplots import make_subplots
+from dash.exceptions import PreventUpdate
 
 
 # Import other modules
@@ -31,30 +32,6 @@ app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
 
 # alphabet = {num:i for num,i in zip(range(1,27),ascii_uppercase)}
-
-# fig = make_subplots(rows=6, cols=5)
-
-# for row_i in range(1,6):
-    # for col_i in range(1,7):
-        # fig.add_trace(
-            # go.Scatter(
-                # x=[1], y=[1],
-                # text=str([row_i,col_i]),
-                # mode="text",
-            # ),
-            # row=row_i, col=col_i
-        # )
-
-
-# fig.update_traces(
-    # textposition='middle center',
-    # textfont_size=20,
-    # showlegend=False,
-    
-# )
-# fig.update_xaxes(showgrid=False,showticklabels=False)
-# fig.update_yaxes(showgrid=False,showticklabels=False)
-
 
 #Subplot creating function
 def create_subplot(row, col, fig, color="gray",text="hi"):
@@ -96,7 +73,7 @@ fig.update_traces(
 fig.update_xaxes(showgrid=False,showticklabels=False)
 fig.update_yaxes(showgrid=False,showticklabels=False)
 fig.update_layout(
-    height=500,
+    height=400,
     width=1000,
     margin=dict(
     l=200,
@@ -112,8 +89,9 @@ fig.update_layout(
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
 
 
-app.layout = html.Div(
+app.layout = dbc.Container(
     [
+        #Row 1
         dbc.Row(
             dbc.Col(
                 html.H3(
@@ -121,91 +99,136 @@ app.layout = html.Div(
                     style={
                         "font-size": "60px",
                         "margin-top": "5px",
-                        "margin-bottom" : "0px",
+                        "margin-bottom" : "5px",
                         "textAlign": "center",
                     },
                 ),    
             )
         ),
+        #Row 2
         dbc.Row(
             dbc.Col(
                 dcc.Graph(
                     id = "plots",
                     figure= fig,
                 ),
-                width={"size": "4","offset":"2"},
+                width={"size": "12","offset": "1"},
                 align="center",
                 
-            )
+            ), 
+            align = "center",
+            justify = "center",
         ),
+        #Row 3
         dbc.Row(
-            dbc.Col(
-                dbc.Input(
-                    id = "input_box",
-                    type="text",
-                    maxlength=5,
-                    placeholder="hello",
-                    invalid=True,
-                    # size="lg",
-                    class_name="mb-3",
-                    value=[],
-                    # pattern=r"[a-z]"
+            [
+                dbc.Col(
+                    dbc.Input(
+                        id = "input_box",
+                        type="text",
+                        maxlength=5,
+                        placeholder="hello",
+                        invalid=False,
+                        class_name="mb-3",
+                        value=[],
+                    ),
+                    width={"size": "4"},
+                    align="center",
                 ),
-                width={"size": "4","offset":4},
-                align="center",
-            )
+                dbc.Col(
+                    dbc.Button(
+                        "Enter",
+                        id = "enter_button",
+                        color="primary",
+                        class_name= "mb-3",
+                        size = "lg",
+                    ),
+                    width={"size": "2"},
+                    align= "center",
+                
+                
+                
+                ),
+            ],
+            align="center",
+            justify="center"
         ),
+        #Row 4
         dbc.Row(
-            dbc.Col(
-                html.Div(
-                    id="text_output",
-                    children="Enter a value",
-                )
-            )
-        )
-    ]
+            [
+                dbc.Col(
+                    html.Div(
+                        id="text_output",
+                        children="Enter a value",
+                    ),
+                ),
+                dbc.Col(
+                    dcc.Store(id="plot_store"),
+                ),
+            ],
+            align="center",
+            justify="center"
+        ),
+    ], 
+    fluid=True
 )
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Callbacks ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
 #Define the word to guess somewhere
-answer = list("lemon")
-
+answer = list("LEMON")
 
 @app.callback(
     Output("plots", "figure"),
     Output("text_output", "children"),
     Input("input_box", "value"),
+    State("plots", "figure"),
 )
 
-def update_plots(word):
+def update_plots(word,figure):
     #Somewhere here it should recieve the plot to update rather than using the global plot 
         #~ Temporaraly explicitly defining the plot as global to prevent an odd lag in inputs otherwise
         #~ Nevermind that isn't what was causing it. Just make sure the word.append isn't empty
     # global fig
     # fig = fig
-    
+        
     #Set the word to 5 letters 
     word = list(word)
     while len(word) <5:
         word.append(" ")
+    word = [x.upper() for x in word]
     
+    #Add the letters to the plot and indicate whether the letters are correct
+        #~ The correctness check needs to go into a different callback that utilises and input button(or other)
+        #~ Also needs a bit more logic to account for letters occuring multiple times
     for guess_letter, ans_letter, num in zip(word,answer,range(0,5)):
-        fig.data[num].text = guess_letter
-        if guess_letter is ans_letter:
-            fig.data[num].marker.update(color="green",symbol = "star")
+        #Add letter to graphs
+        figure["data"][num]["text"] = guess_letter
+        #Correct letter and postion
+        if guess_letter == ans_letter:
+            figure["data"][num]["marker"].update(color="green",symbol = "star")
+        #Correct letter wrong position
         elif guess_letter in answer:
-            fig.data[num].marker.update(color="orange",symbol = "diamond")
+            figure["data"][num]["marker"].update(color="orange",symbol = "diamond")
+        #Wrong letter and position
         else:
-            fig.data[num].marker.update(color="grey",symbol="x")    
-    # print(word)
-    return fig, word
+            figure["data"][num]["marker"].update(color="grey",symbol="x")
+    return figure, word
 
 
+# @app.callback(
+    # Output("plot_store", "data"),
+    # Input("enter_button", "n_clicks"),
+    # State("input_box", "value"),
+    # State("plots", "figure"),
 
+# )
 
+# def update_store(n_clicks,word,figure):
 
+    
+    # return(figure)
 
 
 
